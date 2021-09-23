45185920572550110bc81ade1c93d186509bf896
Pablo-Rovito <pablo.rovito@outlook.com>
2021-09-23 14:14:39 -0300

    ejercicios de Math.floor() resueltos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -50,18 +50,32 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch (color) {
+    case "blue":
+      return "This is blue";
+    case "red":
+      return "This is red";
+    case "green":
+      return "This is green";
+    case "orange":
+      return "This is orange";
+    default:
+      return "Color not found";
+      }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  return (numero===10 ||numero===5);
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  return (20<numero && numero<50)
 }
 
 function esEntero(numero) {
@@ -72,6 +86,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  return (Math.floor(numero)===numero)
 }
 
 function fizzBuzz(numero) {
@@ -79,6 +94,13 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (Math.floor(numero/3)===(numero/3) && Math.floor(numero/5)===(numero/5)) {
+    return "fizzbuzz" }
+  else if (Math.floor(numero/3)===(numero/3)) {
+    return "fizz"}
+  else if (Math.floor(numero/5)===(numero/5)) {
+    return "buzz"}
+  else return numero;
 }
 
 function operadoresLogicos(num1, num2, num3) {
